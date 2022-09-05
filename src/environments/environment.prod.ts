b29ff83b3896751ffdba46a6e01d8d7bfbf3e6bb
Ly Do <lydo1797@gmail.com>
2022-09-04 23:01:30 -0700

    Add deploy script.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: '', // TODO: get url from backend
-  userPoolClientId: '', // TODO: get id from backend
-  userPoolId: '', // TODO: get id from backend
+  userPoolClientId: 'vsmdl7573cvfmnbkb5hueid10', // TODO: get id from backend
+  userPoolId: 'us-west-1_3JajC6owt', // TODO: get id from backend
 };
