fc77f1b4c8818863f841d16678349f6220fc570a
Huy Pham <hpham@snapchat.com>
2024-05-07 21:34:34 -0700

    Fix sorting for customer-list table.

--- src/app/components/customer-list/customer-list.component.ts
+++ src/app/components/customer-list/customer-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { CustomerInfo, OptStatus } from 'src/app/models/customer-info.model';
 import { CustomersService } from 'src/app/services/customers.service';
@@ -9,18 +11,27 @@ import { CustomersService } from 'src/app/services/customers.service';
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss']
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit, AfterViewInit {
 
   OptStatus: any = OptStatus;
   isLoading = false;
-  displayedColumns: string[] = ['phone', 'name', 'optStatus'];
-  dataSource: MatTableDataSource<CustomerInfo> | undefined;
+  displayedColumns: string[] = ['phone', 'createdAt', 'lastUpdatedAt', 'name', 'latestOptStatus'];
+  dataSource: MatTableDataSource<CustomerInfo> = new MatTableDataSource<CustomerInfo>();
+
+  @ViewChild(MatSort) sort: MatSort | undefined;
 
   constructor(private customersService: CustomersService) { }
 
   async ngOnInit() {
     console.log('customer-list: init');
     await this.loadTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    console.log('ng after view init', this.sort);
+    if (this.dataSource) {
+      this.dataSource.sort = this.sort ? this.sort : null;
+    }
   }
 
   async loadTableDataSource() {
@@ -30,6 +41,7 @@ export class CustomerListComponent implements OnInit {
       console.log('customers =', customers);
       // (customers as any[]).forEach(customer => customer.checked = customer.latestOptStatus == OptStatus.IN);
       this.dataSource = new MatTableDataSource<CustomerInfo>(customers);
+      this.dataSource.sort = this.sort!;
     } catch (e: any) {
       // TODO: notify admin
       this.dataSource = new MatTableDataSource<CustomerInfo>();
@@ -55,6 +67,13 @@ export class CustomerListComponent implements OnInit {
       toggle.checked = true;
     } finally {
       element.isSubmitting = false;
+    }
+  }
+
+  announceSortChange(sortState: Sort) {
+    console.log('announceSortChange: ', sortState, this.sort);
+    if (this.dataSource) {
+      this.dataSource.sort = this.sort ? this.sort : null;
     }
   }
 
